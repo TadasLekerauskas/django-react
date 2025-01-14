64adc667049de106f0c3e9c82a7569b871cfbe6b
TadasLekerauskas <159998656+TadasLekerauskas@users.noreply.github.com>
2025-01-14 17:15:49 +0200

    About complete api code
    Finished with the views of api. They send JSON. It will be used later with react.

--- message_project/api/urls.py
+++ message_project/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import UserView
+from .views import ChannelView
+from .views import MessagesView
+from .views import UserChannelView
 
 urlpatterns = [
-    path('', UserView.as_view())
+    path('users', UserView.as_view()),
+    path('channels', ChannelView.as_view()),
+    path('messages', MessagesView.as_view()),
+    path('userChannel', UserChannelView.as_view())
 ]
--- message_project/api/views.py
+++ message_project/api/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import User
+from .models import Channel
+from .models import Messages
+from .models import UserChannel
 from .serializers import UserSerializer
+from .serializers import ChannelSerializer
+from .serializers import MessagesSerializer
+from .serializers import UserChannelSerializer
 
 # Create your views here.
 
 class UserView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class ChannelView(generics.ListAPIView):
+    queryset = Channel.objects.all()
+    serializer_class = ChannelSerializer
+
+class MessagesView(generics.ListAPIView):
+    queryset = Messages.objects.all()
+    serializer_class = MessagesSerializer
+
+class UserChannelView(generics.ListAPIView):
+    queryset = UserChannel.objects.all()
+    serializer_class = UserChannelSerializer
